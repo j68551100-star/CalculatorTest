50947af5f4fda8a56cedbe0e1544f2f71ee85a4b
Jannatul Lima <jannatul0010@gmail.com>
2026-08-13 16:53:52 +0600

    Initial Commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Calculator {
+
+    public void display(int a, int b) {
+
+        Addition addition = new Addition();
+        Subtraction subtraction = new Subtraction();
+        Multiplication multiplication = new Multiplication();
+        Division division = new Division();
+
+        System.out.println("Addition: " + addition.add(a, b));
+        System.out.println("Subtraction: " + subtraction.subtract(a, b));
+        System.out.println("Multiplication: " + multiplication.multiply(a, b));
+        System.out.println("Division: " + division.divide(a, b));
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.display(20, 5);
+    }
+}
---
+++ src/main/java/org/example/Multiplication.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Multiplication {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
